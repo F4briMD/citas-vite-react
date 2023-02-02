05e592cb0ae3a3c038c7179059da1e4add48cef5
F4brimd <f4brimd@hotmail.com>
2023-02-02 19:31:07 -0300

    Cambio header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ function Header() {
 
     return (
       <>
-        <h1 className='font-black text-5xl text-center md:w-2/3 mx-auto'>Seguimieto Paciente {''}
+        <h1 className='font-black text-5xl text-center md:w-2/3 mx-auto'>Seguimieto Paciente {''} Con Github {''}
         <span className='text-indigo-500'>Veterinaria</span>
         </h1> 
       </>
